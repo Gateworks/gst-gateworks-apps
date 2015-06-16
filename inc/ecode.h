8da72bc7e38c47ec6efbbe5168b45564ee6a2e67
Pushpal Sidhu <psidhu@gateworks.com>
2015-06-15 17:50:33 -0700

    Add: Common include file
    This file includes error codes that will be common to all applications under
    this suite of programs.
    
    Signed-off-by: Pushpal Sidhu <psidhu@gateworks.com>

---
+++ inc/ecode.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2015 Pushpal Sidhu <psidhu@gateworks.com>
+ *
+ * Filename: ecode.h
+ * Description: Error codes used by this project suite
+ * Author: Pushpal Sidhu <psidhu@gateworks.com>
+ * Created: Wed May 27 15:02:02 2015 (-0700)
+ * Version: 1.0
+ * Last-Updated: Mon Jun 15 17:46:01 2015 (-0700)
+ *           By: Pushpal Sidhu
+ */
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ECODE_H_
+#define _ECODE_H_
+
+/* General Error Codes */
+#define ECODE_OKAY 0
+#define ECODE_ARGS 1
+#define ECODE_ELEM 2
+#define ECODE_PIPE 3
+#define ECODE_PLAY 4
+#define ECODE_QUIT 5
+#define ECODE_RTSP 6
+
+
+#endif  /* _ECODE_H_ */
+
+/* ecode.h ends here */
